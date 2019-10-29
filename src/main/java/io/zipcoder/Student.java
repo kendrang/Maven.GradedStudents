63746b0f316b588cf5bffef5181ac44d4e738136
kendrang <kendrang@users.noreply.github.com>
2019-10-29 17:08:58 -0400

    set up the student class

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,41 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+
+    String firstName = "";
+    String lastName = "";
+    ArrayList<Double> examScores ;
+
+    public Student (String firstName , String lastName, ArrayList<Double> examScores ){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = examScores;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public ArrayList<Double> getExamScores() {
+        return examScores;
+    }
+
+    public Integer getNumberOfExamsTaken(){
+        return examScores.size();
+    }
+
 }
