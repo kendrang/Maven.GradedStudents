30d348b40dcb0719c3c4ba7bd2712772227985d7
kendrang <kendrang@users.noreply.github.com>
2019-10-30 17:48:32 -0400

    got the average classroom

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,33 @@
 package io.zipcoder;
 
 public class Classroom {
+
+
+    Student[] students;
+
+    public Classroom (int maxNumberOfStudents){
+        students = new Student[maxNumberOfStudents];
+
+    }
+
+    public Classroom(Student[]students){
+        this.students= students;
+
+    }
+
+    public Classroom (){
+        students = new Student[30];
+    }
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public Double getAverageExamScore() {
+        Double addedScores = 0.0;
+        for (int i = 0; i < students.length; i++) {
+
+            addedScores += students[i].getAverageExamScore();
+        }
+        return addedScores / students.length;
+    }
 }
